a9a4b4d6a2388edf74fbe160fa9e1782207e801d
Francieli Martins Rocha <119825620+FrancieliMrtns@users.noreply.github.com>
2023-05-28 16:11:52 -0300

    Update and rename create.sql to Tabelas.sql
    Fiz uns ajustes de acordo com o Dicionário de dados

--- Tabelas.sql
+++ Tabelas.sql
@@ -1,16 +1,10 @@
-CREATE DATABASE mymarket;
-
-SHOW DATABASES;
-
-USE mymarket;
-
 
 CREATE TABLE produtos(
 	prod_id INT auto_increment not null,
 	prod_cod_barras BIGINT UNIQUE,
     prod_nome VARCHAR(30),
     prod_descricao VARCHAR (80),
-    prod_preco INT,
+    prod_preco DECIMAL(6,2),
     prod_imagem VARCHAR(128),
     prod_quantidade INT, 
     primary key(prod_id) 
@@ -19,15 +13,16 @@ CREATE TABLE produtos(
 CREATE TABLE compra_produtos(
 	prod_id INT not null, 
     comp_id INT not null,
-	cmpprd_quantidade BIGINT,
-    cmpprd_valor BIGINT,
+	cmpprd_quantidade INT,
+    cmpprd_valor DECIMAL(6,2),
+    cmpprd_validade DATE,
     primary key(prod_id, comp_id)
 );
 
 CREATE TABLE compras(
-	comp_id BIGINT auto_increment not null,
+	comp_id INT auto_increment not null,
     comp_data DATE,
-    comp_empresa INT,
+    comp_empresa VARCHAR(40),
     primary key(comp_id)
 );
 
@@ -39,10 +34,10 @@ CREATE TABLE categorias(
 );
 
 CREATE TABLE pedido_produto (
-	prod_id INT auto_increment,
+	prod_id INT,
     ped_id INT auto_increment,
 	pprd_quantidade INT,
-    pprd_valor INT,
+    pprd_valor DECIMAL(6,2),
     pprd_observacoes VARCHAR(80),
     primary key(prod_id),
     primary key(ped_id)
@@ -53,7 +48,7 @@ CREATE TABLE pedido_produto (
 CREATE TABLE pedidos(
 	ped_id INT auto_increment,
     ped_nota_fiscal VARCHAR(80),
-    ped_data INT,
+    ped_data DATE,
     end_id INT,
     cli_id INT,
     ped_status VARCHAR(80),
@@ -74,10 +69,10 @@ CREATE TABLE endereco(
 	end_id INT auto_increment,
     end_tipo VARCHAR(10),
     end_logradouro VARCHAR(30),
-    end_numero INT,
+    end_numero VARCHAR(10),
     end_bairro VARCHAR(20),
-    end_complemento VARCHAR(10),
-    end_cidade VARCHAR(20),
+    end_complemento VARCHAR(12),
+    end_cidade VARCHAR(30),
     cli_id INT,
     primary key(end_id)
 );
@@ -90,11 +85,10 @@ CREATE TABLE clientes(
 CREATE TABLE usuarios(
 	usu_id INT auto_increment,
     usu_nome VARCHAR(40),
-    usu_cpf INT,
-    usu_dt_nascimento INT,
-    usu_email VARCHAR(30),
-    usu_senha INT,
+    usu_cpf char(14),
+    usu_dt_nascimento DATE,
+    usu_email VARCHAR(60),
+    usu_senha VARCHAR(20),
     usu_tipo VARCHAR(10),
     primary key(usu_id)
 );
-
